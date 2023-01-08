4341021ae59088465954b54c6ab6b5cb28cab88d
rehatiel <rehatiel@gmail.com>
2023-01-08 09:46:32 -0500

    Update shell.ts
    Responds with actual linux command (no help)

--- src/utils/shell.ts
+++ src/utils/shell.ts
@@ -16,7 +16,7 @@ export const shell = async (
     setHistory('');
   } else if (Object.keys(bin).indexOf(args[0]) === -1) {
     setHistory(
-      `shell: command not found: ${args[0]}. Try 'help' to get started.`,
+      `-bash: ${args[0]}: command not found`,
     );
   } else {
     const output = await bin[args[0]](args.slice(1));
